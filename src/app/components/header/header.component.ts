29900239ea4db39416ef1ff1f6320512b025baff
NadiiaStoiko <nadiia.stoiko@gmail.com>
2021-12-03 18:52:38 +0200

    added sidebar, top panel

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  public isShow = false;
+  public visibility: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public toggle() {
+    this.isShow = !this.isShow;
+    this.visibility = !this.visibility;
   }
-
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,13 +3,110 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  public icons = [
+    {
+      src: '/assets/img/img-sidebar/home.svg',
+      srcActive: '/assets/img/img-sidebar/home _green.svg',
+      alt: 'home',
+      text: 'À Tableau de bord',
+    },
+    {
+      src: '/assets/img/img-sidebar/directory.svg',
+      srcActive: '/assets/img/img-sidebar/directory _green.svg',
+      alt: 'directory',
+      text: 'Dossiers',
+    },
+    {
+      src: '/assets/img/img-sidebar/calendar.svg',
+      srcActive: '/assets/img/img-sidebar/calendar_green.svg',
+      alt: 'calendar',
+      text: 'Session',
+    },
+    {
+      src: '/assets/img/img-sidebar/location.svg',
+      srcActive: '/assets/img/img-sidebar/location _green.svg',
+      alt: 'location',
+      text: 'Provenance',
+    },
+    {
+      src: '/assets/img/img-sidebar/clock_circle.svg',
+      srcActive: '/assets/img/img-sidebar/clock_circle _green.svg',
+      alt: 'clock',
+      text: 'Suivi modifications',
+    },
+    {
+      src: '/assets/img/img-sidebar/user_grey.svg',
+      srcActive: '/assets/img/img-sidebar/user_green.svg',
+      alt: 'user',
+      text: 'Utilisateurs',
+    },
+    {
+      src: '/assets/img/img-sidebar/school.svg',
+      srcActive: '/assets/img/img-sidebar/school _green.svg',
+      alt: 'school',
+      text: 'Formations',
+    },
+    {
+      src: '/assets/img/img-sidebar/copyright.svg',
+      srcActive: '/assets/img/img-sidebar/copyright _green.svg',
+      alt: 'copyright',
+      text: 'BPF',
+    },
+    {
+      src: '/assets/img/img-sidebar/evro.svg',
+      srcActive: '/assets/img/img-sidebar/evro _green.svg',
+      alt: 'money',
+      text: 'Couts',
+    },
+    {
+      src: '/assets/img/img-sidebar/download.svg',
+      srcActive: '/assets/img/img-sidebar/download _green.svg',
+      alt: 'download',
+      text: 'Import Leads',
+    },
+    {
+      src: '/assets/img/img-sidebar/download.svg',
+      srcActive: '/assets/img/img-sidebar/download _green.svg',
+      alt: 'download',
+      text: 'Import IPBX',
+    },
+    {
+      src: '/assets/img/img-sidebar/compare_arrows.svg',
+      srcActive: '/assets/img/img-sidebar/compare_arrows _green.svg',
+      alt: 'arrows',
+      text: 'Compteurs',
+    },
+    {
+      src: '/assets/img/img-sidebar/load.svg',
+      srcActive: '/assets/img/img-sidebar/load _green.svg',
+      alt: 'load',
+      text: 'Relances',
+    },
+    {
+      src: '/assets/img/img-sidebar/setting.svg',
+      srcActive: '/assets/img/img-sidebar/setting _green.svg',
+      alt: 'setting',
+      text: 'Parametres',
+    },
+    {
+      src: '/assets/img/img-sidebar/on.png',
+      srcActive: '/assets/img/img-sidebar/on_green.svg',
+      alt: 'picture',
+      text: 'Déconnexion',
+    },
+  ];
 
-  constructor() { }
+  public activeItem = false;
+  public selectedIcon: any;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public isActive(icon: any) {
+    this.activeItem = !this.activeItem;
+    this.selectedIcon = icon;
   }
-
 }
