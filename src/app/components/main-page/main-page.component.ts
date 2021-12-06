9bc1fabef86682863598116dce5357cb0648afcf
NadiiaStoiko <nadiia.stoiko@gmail.com>
2021-12-06 19:03:46 +0200

    added styles

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent implements OnInit {
+  public itemsList = [
+    { item: 'Pré-valuation 1', id: 1 },
+    { item: 'Pré-valuation 2', id: 2 },
+    { item: 'Pré-valuation 3', id: 3 },
+    { item: 'Pré-valuation 4', id: 4 },
+    { item: 'Pré-valuation 5', id: 5 },
+  ];
 
-  constructor() { }
+  public isSelected = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  public toggle() {
+    this.isSelected !== this.isSelected;
   }
-
 }
